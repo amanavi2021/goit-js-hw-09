6b3b8dbca67fe5659ef619070c5cad3446c1269e
Nadiia Maltseva <amanavi2021@gmail.com>
2023-03-18 23:37:57 +0200

    adds 01 changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerID = null;
 
+stopBtn.disabled= true;
+
+startBtn.addEventListener('click', () => {
+    timerID = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    },1000);
+
+    startBtn.disabled =true;
+    stopBtn.disabled= false;
+
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerID);
+
+    startBtn.disabled =false;
+    stopBtn.disabled= true;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
