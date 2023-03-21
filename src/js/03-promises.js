a8a7d2e006ecfc38c3cb63843d396eccffe1bac8
Nadiia Maltseva <amanavi2021@gmail.com>
2023-03-21 10:07:35 +0200

    Updates 03js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+refs = {
+  firstDelayField:document.querySelector('input[name="delay"]'),
+  delayStepField:document.querySelector('input[name="step"]'),
+  amountField: document.querySelector('input[name="amount"]'),
+  createBtn: document.querySelector('button'),
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
+
+return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+
+  }, delay);
+
+});  
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let delay = Number(refs.firstDelayField.value);
+  const step = Number(refs.delayStepField.value);
+  const amount = Number(refs.amountField.value);
+ 
+  for (let position=1; position<=amount; position+=1) {
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  })
+  delay+=step;
+  
   }
-}
+};
